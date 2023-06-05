9a1dbdbe8bfbc31c65f4942f8132415c2a2f1cad
ahamedzoha <ahamed.zoha@gmail.com>
2023-06-05 13:43:39 +0600

    completed users basic controllers and services

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from "@nestjs/common"
+import { CreateUserDto } from "./dtos/create-user.dto"
+import { UsersService } from "./users.service"
+import { UpdateUserDto } from "./dtos/update-user.dto"
 
-@Controller('users')
-export class UsersController {}
+@Controller("auth")
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Get(":id")
+  findUser(@Param("id") id: number) {
+    return this.usersService.findOne(id)
+  }
+
+  @Get()
+  findAllUsers(@Query("email") email: string) {
+    return this.usersService.find(email)
+  }
+
+  @Post("/signup")
+  createUser(@Body() body: CreateUserDto) {
+    this.usersService.create(body.email, body.password)
+  }
+
+  @Patch(":id")
+  updateUser(@Param("id") id: number, @Body() body: UpdateUserDto) {
+    this.usersService.update(id, body)
+  }
+
+  @Delete(":id")
+  removeUser(@Param("id") id: number) {
+    this.usersService.remove(id)
+  }
+}
