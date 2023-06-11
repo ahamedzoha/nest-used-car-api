7d0f0cea0d5a08c2cbd86df380c9201eed86967c
ahamedzoha <ahamed.zoha@gmail.com>
2023-06-11 13:35:52 +0600

    added report approval modification

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,15 +1,28 @@
-import { Body, Controller, Post, UseGuards } from "@nestjs/common"
+import { Body, Controller, Param, Patch, Post, UseGuards } from "@nestjs/common"
 import { CreateReportDto } from "./dtos/create-report.dto"
 import { ReportsService } from "./reports.service"
 import { AuthGuard } from "src/guards/auth.guard"
+import { CurrentUser } from "src/users/decorators/current-user.decorator"
+import { User } from "src/users/user.entity"
+import { Serialize } from "src/interceptors/serialize.interceptor"
+import { ReportDto } from "./dtos/report.dto"
+import { ApprovedReportDto } from "./dtos/approved-report.dto"
 
 @Controller("reports")
 export class ReportsController {
   constructor(private reportsService: ReportsService) {}
 
-  @UseGuards(AuthGuard)
   @Post()
-  createReport(@Body() body: CreateReportDto) {
-    return this.reportsService.create(body)
+  @UseGuards(AuthGuard)
+  @Serialize(ReportDto)
+  createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {
+    return this.reportsService.create(body, user)
+  }
+
+  @Patch("/:id")
+  @UseGuards(AuthGuard)
+  //   @Serialize(ReportDto)
+  approveReport(@Param("id") id: number, @Body() body: ApprovedReportDto) {
+    return this.reportsService.changeApproval(id, body.approved)
   }
 }
